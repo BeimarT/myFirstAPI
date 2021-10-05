dd8f35fa47dd9e84607053be16c506b55df6cc46
Beim2 <beimar.torrez@etpxavier.com>
2021-10-05 19:32:54 +0200

    lista
    array

--- controllers/arrayLista.js
+++ controllers/arrayLista.js
@@ -1,8 +1,9 @@
 module.exports.arrayLista = (req, res) => {
-    let responseObj ={}
+    let responseObj ={};
     $array = ["Gerard", "Khadija","Alex","Roger","Xavier","Eric","Miquel","Alba","Raul","Marc","Jordi","Marc","Pol"];
     responseObj.alumnos = array;
     responseObj.contadorDeAlumnos = array.lenght;
+    const responseStatus = 200;
 
     res.status(responseStatus).send(responseObj);
 }
