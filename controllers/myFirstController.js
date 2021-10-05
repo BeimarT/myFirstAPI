ed116c15fa4fa1c1c4b1b5fc75a3642d85a38231
Beim2 <beimar.torrez@etpxavier.com>
2021-10-05 19:03:49 +0200

    hello
    aaa

--- controllers/myFirstController.js
+++ controllers/myFirstController.js
@@ -1,4 +1,4 @@
-module.exports.helloWorld = (req, res) => {          // añadir module.exports.helloWorl
+module.exports.helloWorld = (req, res) => {          // añadir module.exports.helloWorl para exportar req = peticion de usuario res = respuesta
     const responseObj = {msg: 'Hello World!'};
     const responseStatus = 200;
     res.status(responseStatus).send(responseObj);
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+require('dotenv');
 
 const myFirstController = require('./controllers/myFirstController');
 
@@ -8,6 +9,6 @@ const myFirstController = require('./controllers/myFirstController');
 
 app.get('/', myFirstController.helloWorld);              // Añadir la ruta de controllers y el nombre del nombre a exportar
 
-app.listen(3000, () =>{
+app.listen(process.env.PORT || 3000, () =>{    // executar app por el puerto determinado
 console.log('My First API running!')
 });
